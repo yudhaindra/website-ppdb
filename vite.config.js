b1c99f6075a760c20d1ffe676d1458ff278365b6
yudhaindra <yudha.indra@ti.ukdw.ac.id>
2025-04-22 15:01:29 +0700

    commit kedua

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/css/sb-admin-2.min.css',
+                'resources/js/sb-admin-2.min.js',
+                'resources/js/demo/chart-area-demo.js',
+                'resources/js/demo/chart-pie-demo.js'],
             refresh: true,
         }),
         tailwindcss(),
