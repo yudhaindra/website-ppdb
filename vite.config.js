3146224827be779bfe5c2c72cda74cd445fcd4db
yudhaindra <yudha.indra@ti.ukdw.ac.id>
2025-05-16 21:22:07 +0700

    update vt

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,40 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // Global CSS
                 'resources/css/main.css',
+                'resources/css/base.css',
+                'resources/css/footer.css',
+                'resources/css/forms.css',
+                'resources/css/header.css',
+                'resources/css/hero.css',
+                'resources/css/navigation.css',
+                'resources/css/preloader.css',
+                'resources/css/registration-pages.css',
+                'resources/css/sections.css',
+                'resources/css/utilities.css',
+                'resources/css/variables.css',
 
+                // Global JS
+                'resources/js/main.js',
+                'resources/js/bootstrap.js',
+
+                // Admin CSS
                 'resources/css/admin/app.css',
-                'resources/js/admin/app.js',
-
                 'resources/css/admin/sb-admin-2.min.css',
-                'resources/js/admin/chart.js',
 
+                // Admin JS
+                'resources/js/admin/app.js',
+                'resources/js/admin/sb-admin-2.min.js',
+                'resources/js/admin/chart.js',
                 'resources/js/admin/jquery.min.js',
                 'resources/js/admin/bootstrap.bundle.min.js',
+
+                // Demo JS
+                'resources/js/chart-area-demo.js',
+                'resources/js/chart-bar-demo.js',
+                'resources/js/chart-pie-demo.js',
+                'resources/js/datatables-demo.js',
             ],
             refresh: true,
         }),
